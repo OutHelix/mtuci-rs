1cfc1b92405614ed34933a11207b5ba47ecde80c
OutHelix <outhelix@gmail.com>
2023-06-24 15:50:53 +0300

    rock paper scissors game

---
+++ minigame/src/main.rs
@@ -0,0 +1,64 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    loop {
+        println!("Choose the hand");
+        println!("1 - Rock, 2 - Paper, 3 - Scissors, 4 - End the game");
+        let bot_hand: u8 = rand::thread_rng().gen_range(1..=3); // генерация случайного выбора
+    
+        // обработка пользовательского ввода
+        let mut user_hand = String::new();
+        io::stdin()
+            .read_line(&mut user_hand)
+            .expect("Failed to read line");
+        
+        // исключение неверного ввода
+        let user_hand: u8 = match user_hand.trim().parse() {
+            Ok(user_hand) => user_hand,
+            Err(_) => continue,
+        };
+
+        // выход из программы
+        if user_hand == 4 {
+            println!("Goodbye!");
+            break;
+        }
+
+        choose(user_hand, bot_hand); // функция сверки выбора
+        println!("--------")
+
+
+    }
+}
+
+fn choose (user_hand: u8, bot_hand: u8) {
+    if user_hand == 1 && bot_hand == 3 {
+        println!("Your opponent have scissors");
+        println!("You win")
+    } else if user_hand == 1 && bot_hand == 2 {
+        println!("Your opponent have paper");
+        println!("You lose")
+    } else if user_hand == 1 && bot_hand == 1 {
+        println!("Your opponent have rock too");
+        println!("Draw!")
+    } else if user_hand == 2 && bot_hand == 3 {
+        println!("Your opponent have scissors");
+        println!("You lose")
+    } else if user_hand == 2 && bot_hand == 2 {
+        println!("Your opponent have paper too");
+        println!("Draw")
+    } else if user_hand == 2 && bot_hand == 1 {
+        println!("Your opponent have rock");
+        println!("You win")
+    } else if user_hand == 3 && bot_hand == 3 {
+        println!("Your opponent have scissors too");
+        println!("Draw!")
+    } else if user_hand == 3 && bot_hand == 2 {
+        println!("Your opponent have paper");
+        println!("You win!")
+    } else if user_hand == 3 && bot_hand == 1 {
+        println!("Your opponent have rock");
+        println!("You lose")
+    }
+}
