85c61f3ba4d2755f8d28a4b1c9c7e03ff6787468
OutHelix <outhelix@gmail.com>
2023-06-24 13:24:55 +0300

    Copied from another repo

---
+++ part2/src/main.rs
@@ -0,0 +1,10 @@
+// Copied from the RustTraining repository
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}   
