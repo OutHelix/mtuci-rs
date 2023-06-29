c24dbbc99ee1a3a9d7a894cdecb7314bd64cd1fc
OutHelix <outhelix@gmail.com>
2023-06-29 18:26:44 +0300

    upd maim.rs

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -8,16 +8,5 @@ mod ownership;
 mod slices;
 
 fn main() {
-    primitive_types::primitive_types5();
-}
-
-fn fizzbuzz() {
-    for i in 1..=100 {
-        match (i % 3, i % 5, i % 15) {
-            (_, _, 0) => println!("FizzBuzz"),
-            (_, 0, _) => println!("Buzz"),
-            (0, _, _) => println!("Fizz"),
-            _ => println!("{}", i),
-        }
-    }
+    
 }
